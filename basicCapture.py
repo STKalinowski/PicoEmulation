35d245cda9ab0b5825e77ad3528674337418ae5c
STKalinowski <kalinowski.stas@gmail.com>
2023-06-30 15:57:08 -0700

    Committing current changes

--- basicCapture.py
+++ basicCapture.py
@@ -5,11 +5,11 @@ import datetime
 import mss
 from pynput import keyboard
 import threading
-import pandas as pd
+import duckdb
 
-key_set = set()
-window_pressed = set()
-key_index_map = {'Key.up': 0, 'Key.down': 1, 'Key.left': 2, 'Key.right': 3, "'c'": 4, "'x'": 5}
+key_index_map = {'Key.up': 1 << 0, 'Key.down': 1 << 1, 'Key.left': 1 << 2, 'Key.right': 1 << 3, "'c'": 1 << 4, "'x'": 1 << 5}
+key_set = 0
+window_pressed = 0
 # Define a lock to synchronize access to the one hot set
 lock = threading.Lock()
 
@@ -43,17 +43,19 @@ def record():
     game = 'celest'
     global window_pressed
     global key_set
-    # Define the screen size and region to capture
-    screen_size = {"top":105, "left":1262, "width":128, "height":128}
-    imgReshape = (128,128)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    connection = duckdb.connect(database='recordings.db')
 
-    #df = pd.DataFrame(columns=['id', 'keys', 'game'])
+    # Recording variables
     totalTime = 3 # Total Time in seconds
     fps = 15 # Rate of frame capture in frames per second
     numOfLoops = totalTime * fps
     sleepTime = 1 / fps
     runs = 100
+
+    # Define the screen size and region to capture
+    screen_size = {"top":105, "left":1262, "width":128, "height":128}
+    imgReshape = (128,128)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
     with mss.mss() as sct:
         for i in range(runs):
@@ -74,10 +76,11 @@ def record():
                 timeToWake = time.perf_counter() + sleepTime
                 while time.perf_counter() < timeToWake:
                     time.sleep(0) 
+
             # Now we take our inputs add them to the DB and save our video 
             now = datetime.datetime.now()
             recordDate = now.strftime("%y%m%d%H%S")
-            vid = cv2.VideoWriter(f'./HoldRes/{recordDate}.mp4', fourcc, float(fps), imgReshape)
+            vid = cv2.VideoWriter(f'./Videos/{recordDate}.mp4', fourcc, float(fps), imgReshape)
 
             for frame in recordFrame:
                 img = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
